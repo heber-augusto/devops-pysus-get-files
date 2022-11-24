f83294aebf936fb0ad4f440c98259fd53913160f
Heber Scachetti <heber.augusto@gmail.com>
2022-11-24 13:08:40 -0300

    Update collect_parameters.py

--- auto-get-files/utils/collect_parameters.py
+++ auto-get-files/utils/collect_parameters.py
@@ -3,14 +3,22 @@ from dateutil.relativedelta import relativedelta
 from dateutil.rrule import rrule, MONTHLY
 import os
 
-
+DEF_START_DATE = (date.today() + relativedelta(months=-6))
+DEF_END_DATE = (date.today())
 def print_parameters():
-    START_DATE = (os.getenv('START_DATE', (date.today() + relativedelta(months=-6)).strftime('%Y-%m-%d')))
-    END_DATE = (os.getenv('END_DATE', (date.today().strftime('%Y-%m-%d'))))
+    START_DATE = (os.getenv('START_DATE', DEF_START_DATE.strftime('%Y-%m-%d')))
+    END_DATE = (os.getenv('END_DATE', DEF_END_DATE.strftime('%Y-%m-%d')))
     STATES = (os.getenv('STATES', 'SP'))
-
-    strt_dt = datetime.strptime(START_DATE,'%Y-%m-%d')
-    end_dt = datetime.strptime(END_DATE,'%Y-%m-%d')
+    try:
+        strt_dt = datetime.strptime(START_DATE,'%Y-%m-%d')
+    except ValueError:
+        strt_dt = DEF_START_DATE                
+        pass
+    try:
+        end_dt = datetime.strptime(END_DATE,'%Y-%m-%d')
+    except ValueError:
+        strt_dt = DEF_END_DATE                
+        pass      
     states = STATES.split(',')
 
     dates = [dt for dt in rrule(MONTHLY, dtstart=strt_dt, until=end_dt)]
