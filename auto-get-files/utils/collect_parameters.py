94831432d6023e16aef93fdee71f98ae02c2471f
Heber Scachetti <heber.augusto@gmail.com>
2022-11-24 13:13:12 -0300

    Update collect_parameters.py

--- auto-get-files/utils/collect_parameters.py
+++ auto-get-files/utils/collect_parameters.py
@@ -9,17 +9,26 @@ def print_parameters():
     START_DATE = (os.getenv('START_DATE', DEF_START_DATE.strftime('%Y-%m-%d')))
     END_DATE = (os.getenv('END_DATE', DEF_END_DATE.strftime('%Y-%m-%d')))
     STATES = (os.getenv('STATES', 'SP'))
+    
+    
+    
+    # validate start date
     try:
         strt_dt = datetime.strptime(START_DATE,'%Y-%m-%d')
     except ValueError:
         strt_dt = DEF_START_DATE                
         pass
+    # validate end date
     try:
         end_dt = datetime.strptime(END_DATE,'%Y-%m-%d')
     except ValueError:
         end_dt = DEF_END_DATE                
-        pass      
-    states = STATES.split(',')
+        pass
+    # validate states
+    if STATES == '':
+        states = ['SP',]
+    else:
+        states = STATES.split(',')
 
     dates = [dt for dt in rrule(MONTHLY, dtstart=strt_dt, until=end_dt)]
     for state in states:
