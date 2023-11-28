4f20f378c7e79c96d2629b88bbfbd53956dbb5c7
Heber Scachetti <heber.augusto@gmail.com>
2023-11-28 10:42:12 -0300

    Update collect_from_ftp.sh

--- auto-get-files/collect_from_ftp.sh
+++ auto-get-files/collect_from_ftp.sh
@@ -33,6 +33,7 @@ else
 fi
 FILE_PATH=$1
 DIR_FILES_DEST=$2
+wget --help
 wget -P $DIR_FILES_DEST --user=Anonymous password= ftp://ftp.datasus.gov.br${FILE_PATH}
 echo "Arquivo \$1 coletado com sucesso!"
 exit $vrc
