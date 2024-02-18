36d85153f68487a64a8b027a13f87be6c7d638ae
Heber Scachetti <heber.augusto@gmail.com>
2024-02-18 19:07:15 -0300

    Create mount_google_drive.sh

---
+++ auto-get-files/bin/mount_google_drive.sh
@@ -0,0 +1,12 @@
+add-apt-repository --yes ppa:alessandro-strada/ppa
+apt-get update
+apt-get install google-drive-ocamlfuse
+
+mkdir "${1}/datalake"
+mkdir "${1}/gdfuse"
+
+google-drive-ocamlfuse -xdgbd -label monitor -serviceaccountpath "${4}" -serviceaccountuser "${2}"
+
+python ${1}/auto-get-files/utils/gdfuse_config.py -c "${1}/gdfuse/monitor/config" -t "${3}"
+
+google-drive-ocamlfuse -cc -label monitor ${1}/datalake
