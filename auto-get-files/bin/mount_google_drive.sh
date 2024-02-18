0ad895d4ddd11ec988bc06cf612d4fe25166f0fc
Heber Scachetti <heber.augusto@gmail.com>
2024-02-18 19:54:17 -0300

    Update mount_google_drive.sh

--- auto-get-files/bin/mount_google_drive.sh
+++ auto-get-files/bin/mount_google_drive.sh
@@ -2,13 +2,13 @@ sudo add-apt-repository --yes ppa:alessandro-strada/ppa
 sudo apt-get update
 sudo apt-get install google-drive-ocamlfuse
 
-mkdir "${1}/datalake"
-chmod o+w "${1}/datalake"/.
-mkdir "${1}/gdfuse"
-chmod o+w "${1}/gdfuse"/.
+mkdir "${1}/output-files/datalake"
+chmod o+w "${1}/output-files/datalake"/.
+mkdir "${1}/output-files/gdfuse"
+chmod o+w "${1}/output-files/gdfuse"/.
 
 google-drive-ocamlfuse -xdgbd -label monitor -serviceaccountpath "${4}" -serviceaccountuser "${2}"
 
-python3 ${1}/auto-get-files/utils/gdfuse_config.py -c "${1}/gdfuse/monitor/config" -t "${3}"
+python3 ${1}/auto-get-files/utils/gdfuse_config.py -c "${1}/output-files/gdfuse/monitor/config" -t "${3}"
 
-google-drive-ocamlfuse -cc -label monitor ${1}/datalake
+google-drive-ocamlfuse -cc -label monitor ${1}/output-files/datalake
